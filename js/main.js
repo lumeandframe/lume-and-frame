b94a80ee03552c6e4875d88577eef18c4a764d8a
lumeandframe <lumeandframe@users.noreply.github.com>
2026-07-29 21:09:57 +0000

    deploy: 2f7da92a2a3982fd800ed7812488de5001ce623e

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+==================================================
+LUME & FRAME
+Main Interaction Layer
+
+Luce. Forma. Silenzio.
+==================================================
+*/
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+
+    console.log(
+      "LUME & FRAME — Luce. Forma. Silenzio."
+    );
+
+  }
+);
